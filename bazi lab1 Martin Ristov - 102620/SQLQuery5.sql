24adcef7ec121f4ead42526ef5b6a9bccacc5788
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 02:00:37 +0200

    Add files via upload

---
+++ bazi lab1 Martin Ristov - 102620/SQLQuery5.sql
@@ -0,0 +1,4 @@
+INSERT INTO Sluziteli(ID,IME,PREZIME,DOLZNOST,EGN,TELEFONSKI_BROJ,ADRESA,GRAD,MAIL,DATA_VLEZ,DATA_IZLEZ)
+VALUES
+(4,'Marko','Jankovski','Cistas','Nadgleduvac',071519941,'Krste Misirkov 51','Skopje','markojankovski@gmail.com','2023.10.05','2023.11.06'),
+(5,'Ana','Avramova','Sekretarka','CEO',078777950,'Ulov Palme 70','Shtip','anaavramova44@gmail.com','2023.01.15','2023.03.17');
